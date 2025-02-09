b4f3553955da9d02ac7f93181cca9f6424f3a3fa
Nikita <108035712+sobakasbakbaka@users.noreply.github.com>
2025-02-09 22:15:40 +0500

    update main component text to reflect test change

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,6 @@ import { createRoot } from "react-dom/client";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <div>Hello</div>
+    <div>Test Change</div>
   </StrictMode>
 );
