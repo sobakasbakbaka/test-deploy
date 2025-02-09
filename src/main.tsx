20846857d9a832a36980b99388526cbe2d318b55
Nikita <108035712+sobakasbakbaka@users.noreply.github.com>
2025-02-09 23:11:49 +0500

    feat: add Vite environment type definitions and enable class fields in TypeScript configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-// @ts-ignore
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
 import { App } from "./App";
